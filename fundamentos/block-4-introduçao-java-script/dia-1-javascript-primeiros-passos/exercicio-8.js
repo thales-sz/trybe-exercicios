56391bfae7cccf210e31f88bfe6dd3b88b82aaaa
Thales Chagas <thales.souz@outlook.com>
2022-03-07 20:31:26 -0300

    exercicio/4.1 - n8 e n9

---
+++ fundamentos/block-4-introduçao-java-script/dia-1-javascript-primeiros-passos/exercicio-8.js
@@ -0,0 +1,12 @@
+// Caso 1 numero seja par retorna 'true'
+let a,b,c,result;
+a = 3; b = 5; c = 6; result = 0;
+
+if ( a%2 === 0 || b%2 === 0 || c%2 === 0 ) {
+    result = true;
+    console.log(result);
+}
+else {
+    result = false;
+    console.log(result);
+}
---
+++ fundamentos/block-4-introduçao-java-script/dia-1-javascript-primeiros-passos/exercicio-9.js
@@ -0,0 +1,12 @@
+// Caso 1 numero seja impar retorna 'true'
+let a,b,c,result;
+a = 8; b = 6; c = 6; result = 0;
+
+if ( a%2 !== 0 || b%2 !== 0 || c%2 !== 0 ) {
+    result = true;
+    console.log(result);
+}
+else {
+    result = false;
+    console.log(result);
+}
