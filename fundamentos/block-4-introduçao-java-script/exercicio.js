e649e517e4eec6960ae785b8f25df2fcf3ef5d19
Thales Chagas <thales.souz@outlook.com>
2022-03-07 18:29:12 -0300

    caminho para exercicio4.1

---
+++ fundamentos/block-4-introduçao-java-script/exercicio.js
@@ -0,0 +1,11 @@
+const myName = 'Thales';
+const birthCity = 'Volta Redonda';
+let birthYear = 2000;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Arrozal';
