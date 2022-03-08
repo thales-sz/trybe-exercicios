4c0a743d65a1c25f69f3b50125a43a71c4cec7c3
Thales Chagas <thales.souz@outlook.com>
2022-03-08 15:42:32 -0300

    Feito exercicio/4.2 - n1,n2 e n3

---
+++ fundamentos/block-4-introduçao-java-script/dia-1-javascript-primeiros-passos/exercicio-2.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (count = 0; count < numbers.length; count += 1){
+    soma = soma + numbers[count];
+}
+console.log('Soma:', soma);
