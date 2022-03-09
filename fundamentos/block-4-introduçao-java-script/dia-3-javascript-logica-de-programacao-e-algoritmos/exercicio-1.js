ffd0f6299c5624c010f1094a78e0e8b5cde509b4
Thales Chagas <thales.souz@outlook.com>
2022-03-09 15:45:58 -0300

    exercicio/4.3 - n1 e n2

---
+++ fundamentos/block-4-introduçao-java-script/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -0,0 +1,9 @@
+// Algoritmo que calcula o fatorial de 10;
+
+let valorFatorado = 10;
+let result = 1;
+
+for (index = 1; index <= valorFatorado; index++) {
+    result = result * index;
+}
+console.log(result);
