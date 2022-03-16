659538ba187760408abd8460cf31028904fe269e
Thales Chagas <thales.souz@outlook.com>
2022-03-16 20:25:51 -0300

    exercicio/5.3 - numero 2 e 3

--- fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,28 +1,92 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado',]
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  createDaysOfTheWeek();
-// Adicionar os dias do mes de dezembro no calendario
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
-  const dezDays = document.querySelector('#days');
-  console.log(dezDays);
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
 
-  for (let index1 = 0; index1 < dezDaysList.length; index1 += 1) {
-      const day = dezDaysList[index1];
-      const dayItem = document.createElement('li');
-      dayItem.innerHTML = day;
-
-      dezDays.appendChild(dayItem);
+    weekDaysList.appendChild(dayListItem);
   }
-// termina aqui
+}
+
+createDaysOfTheWeek();
+// Numero 1 - Adicionar os dias do mes de dezembro no calendario
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+const dezDays = document.querySelector("#days");
+
+for (let index1 = 0; index1 < dezDaysList.length; index1 += 1) {
+  const day = dezDaysList[index1];
+  const dayItem = document.createElement("li");
+  dayItem.className = "day";
+  if (
+    index1 === 5 ||
+    index1 === 5 + 7 ||
+    index1 === 5 + 14 ||
+    index1 === 5 + 21
+  ) {
+    dayItem.className = "day friday";
+  }
+  if (day === 24 || day === 31) {
+    dayItem.className = "day holiday";
+  }
+  if (day === 25) {
+    dayItem.className = "day friday holiday";
+  }
+  dayItem.innerHTML = day;
+
+  dezDays.appendChild(dayItem);
+}
+// termina aqui numero 1
+
+// Numero 2 - Adicionar botão Feriados.
+
+let text = "Feriados";
+const paiDoBtn = document.querySelector(".buttons-container");
+
+function btnFeriados(text) {
+  const buttonName = text;
+  const button = document.createElement("button");
+  button.id = "btn-holiday";
+  button.innerHTML = buttonName;
+
+  paiDoBtn.appendChild(button);
+}
+btnFeriados(text);
+// termina aqui numero 2
+
+// Numero 3 - Adicionar evento de click ao botão feriados
+
+function createClickEventForButton() {
+  const button = document.querySelector("#btn-holiday");
+  button.addEventListener("click", clickedButton);
+}
+
+function clickedButton(evento) {
+  const feriados = document.querySelectorAll(".holiday");
+  if (feriados[0].style.backgroundColor === "red") {
+    for (let i = 0; i < feriados.length; i += 1) {
+      feriados[i].style.backgroundColor = "rgb(238,238,238)";
+    }
+  } else {
+    for (let i = 0; i < feriados.length; i += 1) {
+      feriados[i].style.backgroundColor = "red";
+    }
+  }
+}
+createClickEventForButton();
+
+// termina numero 3 aqui
