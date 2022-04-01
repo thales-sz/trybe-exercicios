fda0432c9c4e96e89f1595d77a4f65a050fcd613
Thales Chagas <thales.souz@outlook.com>
2022-04-01 18:37:51 -0300

    Exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,13 +1,9 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const ordenar = (oddsAndEvens) => {
+    oddsAndEvens.sort((a,b) => a - b);
+    return oddsAndEvens;
 }
 
-testingScope(false);
+
+console.log(ordenar(oddsAndEvens)); // será necessário alterar essa linha 😉
