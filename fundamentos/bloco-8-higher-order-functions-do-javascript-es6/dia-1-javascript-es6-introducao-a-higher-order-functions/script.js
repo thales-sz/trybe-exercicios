d65fdf9d21541fe68d92aaf9de9ddbb337b99a19
Thales Chagas <thales.souz@outlook.com>
2022-04-07 18:37:04 -0300

    Exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,18 @@
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const createData = (nomeCompleto) => {
+  email = `${nomeCompleto.replace(' ', '_').toLowerCase()}@trybe.com`
+  const obj = {
+    nomeCompleto,
+    email,
+  };
+  return obj;
+};
+console.log(newEmployees(createData));
