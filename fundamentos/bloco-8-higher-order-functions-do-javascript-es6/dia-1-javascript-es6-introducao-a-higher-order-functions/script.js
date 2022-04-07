bfdcbb9d8f19a4f66e2c84b8cad7d2ce4a71461b
Thales Chagas <thales.souz@outlook.com>
2022-04-07 20:07:40 -0300

    Exercicio bonus acredito q esteja certo

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -20,7 +20,7 @@ const createData = (nomeCompleto) => {
 // Exercicio 2
 
 const sorteio = (number, verificaSorteio) => {
-  const numbSorteado = Math.floor(Math.random()*(6 - 1) + 1) ;
+  const numbSorteado = Math.floor(Math.random()*(6 - 1) + 1);
   const resultado = verificaSorteio(number, numbSorteado);
   return resultado;
 }
@@ -50,3 +50,84 @@ const correção = (gabarito, respAluno) => {
   }
   return nota;
 };
+
+// Exercicio Bônus
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = () => {
+  const damage = Math.floor(Math.random()*((dragon.strength + 1) - 15) + 15);
+  return damage;
+}
+
+const warriorDamage = () => {
+  const damage = Math.floor(Math.random()*(((warrior.strength * warrior.weaponDmg) + 1)  - warrior.strength) + warrior.strength);
+  return damage;
+}
+
+const mageDamage = () => {
+  if (mage.mana < 15) {
+    return 'Not enough mana!';
+  }
+  const usedMana = mage.mana -= 15;
+  const damage = {
+    dano: Math.floor(Math.random()*(((mage.intelligence * 2) + 1) - mage.intelligence) + mage.intelligence),
+    mana: usedMana,
+  }
+  return damage;
+}
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+  warTurn: warriorTurn = (warriorDamage) =>{
+    const danoTurno = warriorDamage();
+    warrior.damage = danoTurno;
+    dragon.healthPoints -= danoTurno;
+    return 'Turno Warrior finalizado!';
+  },
+  magTurn: mageTurn = (mageDamage) => {
+    const turno = mageDamage();
+    mage.damage = turno.dano;
+    dragon.healthPoints -= turno.dano;
+    mage.mana = turno.mana;
+    return 'Turno Mage finalizado!';
+  },
+  dragTurn: dragonTurn = (dragonDamage) => {
+    const danoTurno = dragonDamage();
+    dragon.damage = danoTurno;
+    mage.healthPoints -= danoTurno;
+    warrior.healthPoints -= danoTurno;
+    return 'Turno Dragon finalizado!';
+  },
+  turn: turnoFinalizado = (battleMembers, mago, warr, drag) => {
+    gameActions.magTurn(mago);
+    gameActions.warTurn(warr);
+    gameActions.dragTurn(drag);
+    return battleMembers;
+  }
+};
+console.log(gameActions.turn(battleMembers, mageDamage, warriorDamage, dragonDamage));
+
+
+
+
