e965dd9af8f5ff046ba0a377dddf72c5c36213a5
Thales Chagas <thales.souz@outlook.com>
2022-04-12 10:36:53 -0300

    Exercicio 7 - finalizado 1000%

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -4,7 +4,7 @@ const books = [
     name: 'As Crônicas de Gelo e Fogo',
     genre: 'Fantasia',
     author: {
-      name: 'George R. R. Martin',
+      name: 'George. R. R. H. Martin',
       birthYear: 1948,
     },
     releaseYear: 1991,
@@ -54,7 +54,7 @@ const books = [
     name: 'O Chamado de Cthulhu',
     genre: 'Terror',
     author: {
-      name: 'H. P. Lovecraft',
+      name: 'Humberto. P. O. Lovecraft',
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -65,10 +65,25 @@ const books = [
 
 // Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais (terminam com um ponto).
 
+// const expectedResult = 'O Senhor dos Anéis';
+
+// function authorWith3DotsOnName() {
+//   const result = books.find((livro) => livro.author.name[1] === '.' && livro.author.name[4] === '.' && livro.author.name[7] === '.');
+//   return result.name;
+// }
+// console.log(authorWith3DotsOnName());
+
 const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
-  const result = books.find((livro) => livro.author.name[1] === '.' && livro.author.name[4] === '.' && livro.author.name[7] === '.');
-  return result.name;
+  return books.filter((book) => {
+    const livroSplit = book.author.name.split(' ');
+    livroSplit.length = 3;
+    if (livroSplit[0].length === 2 && livroSplit[1].length === 2 && livroSplit[2].length === 2) {
+      return livroSplit.filter((word) => word.endsWith('.')).length === 3;
+    }
+  })
 }
-console.log(authorWith3DotsOnName());
+
+
+console.log(authorWith3DotsOnName()[0].name);
